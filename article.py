cba74b91799b4948b0e87ad9c699f6abf2c45d48
D-108 <therealdensentrated+gitcommits@gmail.com>
2024-10-13 05:35:24 -0700

    Implemented article class, Finished script

--- article.py
+++ article.py
@@ -1,3 +1,5 @@
+import json
+
 class article:
     def __init__(self, title, author, data, genre, content):
         self.title = title
@@ -9,3 +11,12 @@ class article:
     def display(self):
         print(f"title: {self.title}\nauthor: {self.author}\ngenre: {self.genre}\ndate: {self.date}\n")
         #print(self.content)
+
+    def to_dict(self):
+        return {"title": self.title, "author": self.author, "genre": self.genre, "date": self.date, "content": self.content}
+    
+    def to_json(self):
+        return json.dumps(self.to_dict())
+    
+    def to_csv(self):
+        return f'"{self.title}","{self.author}","{self.genre}","{self.date}","{self.content}"'
--- script.py
+++ script.py
@@ -2,8 +2,42 @@ import requests
 from article import article
 from bs4 import BeautifulSoup
 
-def scroll_through():
-    """scrolls through a sectionn of the website and scrapes the articles until it cannot anymore"""
+def scroll_through(url):
+    """scrolls through a sectionn of the website and scrapes the articles until it cannot anymore, returns an array of links"""
+    # uses find() to get all of the links for an article
+    # sees if there is a next page
+    # do while loop
+
+    isNext = True
+    links = []
+    counter = 1
+
+    while True: 
+        # gets the text of the webpage
+        webpage = requests.get(url)
+        webpagetext = webpage.text
+        soup = BeautifulSoup(webpagetext, 'html.parser')
+
+        # finds all of the article links and appends them to the list
+        for chunk in soup.find_all('h2', 'post-title'):
+            link = chunk.find('a')['href']
+            links.append(link)
+                
+        # end if there is no next page  
+        isNext = soup.find('a', 'next page-numbers') is not None
+
+        if not isNext:
+            break
+
+        # updates the terminal
+        print(f"scraped page {counter}")
+        counter += 1
+
+        # updates the url to the next page
+        url = soup.find('a', 'next page-numbers')['href']
+
+    return links
+
 
 def scrape_article(url, genre):
     """scrapes the article from the website, and returns an article object"""
@@ -17,15 +51,46 @@ def scrape_article(url, genre):
     byline = soup.find('div', 'post-byline').text
     author = byline.split("on")[0][3:]
     date = byline.split("on")[1]
-    content=soup.find('div', 'post-content').text
+    content = soup.find('div', 'post-content').text.replace('\n', '')
 
     # creates an article object and returns it
     return article(title, author, date, genre, content)
 
+def get_articles(links, genre):
+    """takes an array of links and a genre and returns an array of article objects"""
+    print("getting articles for " + genre)
+    articles = []
+    counter = 1
+    for link in links:
+        articles.append(scrape_article(link, genre))
+        print(f"scraped article {counter} out of {len(links)}")
+        counter += 1
+    return articles
+
 def main(): 
-    print("Hello World")
-    sample_article = scrape_article("http://eic.opalstacked.com/masters-at-work-mills-mfa-artists-discuss-the-show-and-their-inspiration/", "Arts & Entertainment")
-    sample_article.display()
+
+    # gets links for every categroy of article
+    news = scroll_through('http://eic.opalstacked.com/category/news/')
+    arts_and_entertainment = scroll_through('http://eic.opalstacked.com/category/ae/')
+    opinions = scroll_through('http://eic.opalstacked.com/category/opinions/')
+    health = scroll_through('http://eic.opalstacked.com/category/health/')
+    science_and_tech = scroll_through('http://eic.opalstacked.com/category/science-technology/')
+
+    articles = []
+    articles.extend( get_articles(news, "news"))
+    articles.extend(get_articles(arts_and_entertainment, "arts and entertainment"))
+    articles.extend( get_articles(opinions, "opinions"))
+    articles.extend( get_articles(health, "health"))
+    articles.extend( get_articles(science_and_tech, "science and technology"))
+
+    # puts articles in a csv file
+    #with open('article.csv', 'w') as f:
+    #    f.write("title,author,genre,date,content\n")
+
+    with open('articles.csv', 'a') as f:
+        for article in articles:
+            f.write(article.to_csv() + '\n')
+
 
 if __name__=="__main__":
     main()
